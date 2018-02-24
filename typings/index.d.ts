0a4575092c6d8952a8dea729dc7d209b7a126166
Kyra <kyranet@users.noreply.github.com>
2018-02-24 02:04:37 +0100

    Added typings

---
+++ typings/index.d.ts
@@ -0,0 +1,27 @@
+declare module 'enmap-level' {
+
+	import { Level } from 'native-level-promise';
+
+	export class EnmapLevel<K, V> {
+		public features: {
+			multiProcess: false;
+			complexTypes: false;
+			keys: 'single';
+		};
+		public name: string;
+		public dataDir: string;
+		public path: string;
+		public db: Level;
+		private defer: Promise;
+
+		public init(enmap: Map): Promise;
+		public close(): void;
+		public set(key: K, value: V): void;
+		public setAsync(key: K, value: V): Promise<void>;
+		public delete(key: K): void;
+		public deleteAsync(key: K): Promise<void>;
+		private ready(): void;
+		private validateName(): void;
+	}
+
+}
